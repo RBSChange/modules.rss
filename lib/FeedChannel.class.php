a389904a314519f555a219c3797cf584c64de29f
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-07-07 17:55:00 +0200

    [FIX] Update backoffice edition [FIX] Error on invalid rss feed parsing

--- lib/FeedChannel.class.php
+++ lib/FeedChannel.class.php
@@ -13,23 +13,25 @@ class rss_FeedChannel
 	public function __construct($xml, $richContentLevel)
 	{
 		$doc = new DOMDocument();
-		$doc->loadXML($xml);
-		
+		if ($doc->loadXML($xml) === false)
+		{
+			return;
+		}
 		$chanel = $doc->getElementsByTagName('channel')->item(0);
 		if ($chanel !== null)
 		{
 			$title = $chanel->getElementsByTagName('title')->item(0);
 			if ($title !== null)
 			{
-				$this->title = $title->textContent;	
+				$this->title = $title->textContent;
 			}
 			$link = $chanel->getElementsByTagName('link')->item(0);
 			if ($link !== null)
 			{
-			    $this->link = $link->textContent;
+				$this->link = $link->textContent;
 			}
 			
-			foreach ($chanel->getElementsByTagName('item') as $nodeItem) 
+			foreach ($chanel->getElementsByTagName('item') as $nodeItem)
 			{
 				$item = new rss_FeedItem($nodeItem, $richContentLevel, $this);
 				$this->items[] = $item;
@@ -44,7 +46,7 @@ class rss_FeedChannel
 	
 	public function getLink()
 	{
-	    return $this->link;
+		return $this->link;
 	}
 	
 	public function getItems($start = 0, $count = 0)
@@ -57,13 +59,29 @@ class rss_FeedChannel
 		return array_slice($this->items, $start, $count);
 	}
 	
+	/**
+	 * @return boolean
+	 */
+	public function hasItems()
+	{
+		return count($this->items) > 0;
+	}
+	
+	/**
+	 * @return integer
+	 */
+	public function length()
+	{
+		return count($this->items);
+	}
+	
 	public function sliceItems($start = 0, $count = 0)
 	{
 		if ($start > 0 || $count > 0)
 		{
 			$this->items = array_slice($this->items, $start, $count);
 		}
-	}	
+	}
 	
 	public function getFeedType()
 	{
@@ -78,27 +96,29 @@ class rss_FeedChannel
 	public static function mergeItems($channelArray, $title = null)
 	{
 		$finalChanel = null;
-                $items = array();
-                $itemCount = 0;
-		foreach ($channelArray as $channel) 
+		$items = array();
+		$itemCount = 0;
+		foreach ($channelArray as $channel)
 		{
 			if ($finalChanel === null)
 			{
 				$finalChanel = $channel;
-                                if ($title !== null)
-                                {
-                                    $finalChanel->title = $title;
-                                }
+				if ($title !== null)
+				{
+					$finalChanel->title = $title;
+				}
 			}
-                        foreach ($channel->items as $item)
-                        {
-                            $itemCount++;
-                            /* @var $item rss_FeedItem */
-                            $items[date_Calendar::getInstance(date_Converter::convertDateToLocal($item->getDate()))->getTimestamp().'-'.  str_pad($itemCount, 5, '0')] = $item;
-                        }
+			foreach ($channel->items as $item)
+			{
+				$itemCount ++;
+				/* @var $item rss_FeedItem */
+				$items[date_Calendar::getInstance(
+						date_Converter::convertDateToLocal($item->getDate()))->getTimestamp() . '-' . str_pad(
+						$itemCount, 5, '0')] = $item;
+			}
 		}
-                krsort($items);
-                $finalChanel->items = $items;
+		krsort($items);
+		$finalChanel->items = $items;
 		return $finalChanel;
 	}
 }
--- lib/blocks/BlockFeedAction.class.php
+++ lib/blocks/BlockFeedAction.class.php
@@ -14,43 +14,51 @@ class rss_BlockFeedAction extends website_BlockAction
 	 */
 	public function execute($request, $response)
 	{
-                $document = $this->getDocumentParameter();
-		if ($document === null || !$document->isPublished())
+		if ($this->isInBackofficeEdition())
 		{
 			return website_BlockView::NONE;
 		}
-                if ($document instanceof rss_persistentdocument_feed)
-                {
-                    $feeds = array($document);
-                }
-                else if ($document instanceof rss_persistentdocument_feedgroup)
-                {
-                    $feeds = $document->getPublishedFeedArray();
-                }
-                $channels = array();
-                foreach ($feeds as $feed)
-                {
-                    $xmlContent = $feed->getDocumentService()->getRawContent($feed);
-                    if ($xmlContent !== null)
-                    {
-                        $channels[] = new rss_FeedChannel($xmlContent, $this->getConfiguration()->getCleanLevel());
-                    }
-                    
-                }
-                if (count($channels) === 0)
-                {
-                     return website_BlockView::ERROR;
-                }
-                $request->setAttribute('document', $document);
-                $request->setAttribute('channel', rss_FeedChannel::mergeItems($channels));
+		
+		$document = $this->getDocumentParameter();
+		if ($document === null || ! $document->isPublished())
+		{
+			return website_BlockView::NONE;
+		}
+		if ($document instanceof rss_persistentdocument_feed)
+		{
+			$feeds = array($document);
+		}
+		else if ($document instanceof rss_persistentdocument_feedgroup)
+		{
+			$feeds = $document->getPublishedFeedArray();
+		}
+		$channels = array();
+		foreach ($feeds as $feed)
+		{
+			$xmlContent = $feed->getDocumentService()->getRawContent($feed);
+			if ($xmlContent !== null)
+			{
+				$c = new rss_FeedChannel($xmlContent, $this->getConfiguration()->getCleanLevel());
+				if ($c->hasItems())
+				{
+					$channels[] = $c;
+				}
+			}
+		}
+		if (count($channels) === 0)
+		{
+			return website_BlockView::ERROR;
+		}
+		$request->setAttribute('document', $document);
+		$request->setAttribute('channel', rss_FeedChannel::mergeItems($channels));
 		$titles = array();
 		foreach ($channels as $channel)
 		{
-		    $request->setAttribute($channel->getTitle(), $channel->getLink());
-		    $titles[] = $channel->getTitle();
+			$request->setAttribute($channel->getTitle(), $channel->getLink());
+			$titles[] = $channel->getTitle();
 		}
 		$request->setAttribute('title', implode(", ", $titles));
-                $request->setAttribute('backoffice', $this->isInBackofficeEdition());
+		$request->setAttribute('backoffice', $this->isInBackofficeEdition());
 		
 		return $this->getConfiguration()->getDisplayMode();
 	}
